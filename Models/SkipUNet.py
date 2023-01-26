ce190b29de348771b143d670300f687fa1bc15d5
Thakshayan <thakshayan007@gmail.com>
2023-01-26 11:47:35 +0530

    skipnet

--- Models/SkipUNet.py
+++ Models/SkipUNet.py
@@ -1,252 +1,115 @@
+
+from torch.nn.modules.conv import Conv2d
 import torch
 import torch.nn as nn
-from typing import Dict, Type, Union, Sequence, List
+
+def double_conv(in_channels, out_channels):
+    return nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, 3, padding=1),
+        nn.ReLU(inplace=True),
+        nn.Conv2d(out_channels, out_channels, 3, padding=1),
+        nn.ReLU(inplace=True)
+    )   
 
 
-CONV: Dict[int, Union[Type[nn.Conv2d], Type[nn.Conv3d]]] = {2: nn.Conv2d, 3: nn.Conv3d}
-DECONV: Dict[int, Union[Type[nn.ConvTranspose2d], Type[nn.ConvTranspose3d]]] = {2: nn.ConvTranspose2d, 3: nn.ConvTranspose3d}
-DOWN: Dict[int, Union[Type[nn.MaxPool2d], Type[nn.MaxPool3d]]] = {2: nn.MaxPool2d, 3: nn.MaxPool3d}
-DROPOUT: Dict[int, Union[Type[nn.Dropout2d], Type[nn.Dropout3d]]] = {2: nn.Dropout2d, 3: nn.Dropout3d}
-ACT: Dict[str, Union[Type[nn.ReLU],Type[nn.LeakyReLU],Type[nn.PReLU]]] = {'relu': nn.ReLU, 'leaky': nn.LeakyReLU, 'prelu': nn.PReLU}
-NORM: Dict[int, Dict[str,Union[Type[nn.modules.batchnorm._BatchNorm],Type[nn.modules.instancenorm._InstanceNorm],Type[nn.GroupNorm]]]] = {
-    2: {
-        'batch': nn.BatchNorm2d,
-        'instance': nn.InstanceNorm2d,
-        'group': nn.GroupNorm,
-    },
-    3: {
-        'batch': nn.BatchNorm3d,
-        'instance': nn.InstanceNorm3d,
-        'group': nn.GroupNorm,
-    }
-}
+class ModifiedNet(nn.Module):
 
-
-class Block(nn.Module):
-    def __init__(
-        self,
-        in_channels: int,
-        out_channels: int,
-        dim: int,
-        kernel_size: int = 3,
-        stride: int = 1,
-        padding: int = 1,
-        bias: bool = False,
-        dropout: float = 0,
-        act: str = "relu",
-        norm: str = "batch",
-        num_group: int = 6, #8,
-        pre_norm: bool = False,
-        res: bool = False,
-        down: bool = False,
-    ) -> None:
-        '''Base Block for UNet-Like network
-        Args:
-            in_channels: Number of channels in the input image.
-            out_channels: Number of channels produced by the convolution.
-            dim: 2D or 3D convolution.
-            kernel_size: Size of the convolving kernel.
-            stride: Stride of the convolution.
-            padding: Zero-padding added to both sides of the input.
-            bias: If ``True``, adds a learnable bias to the output.
-            dropout: Dropout rate.
-            act: Activation function of Block.``relu``,``prelu`` or ``leaky``.
-            norm: Normalization type.``batch``,``group`` or ``instance``.
-            num_group: number of groups to separate the channels into
-            pre_norm: If true, normalization->activation->convolution.
-            res: If true, set residual-connection to block.
-            down: If true, first conv layer used as downsample layer
-        '''
+    def __init__(self):
         super().__init__()
-        self._check_param(dim, act, norm)
-        self.res = res
-        stride1 = stride if not down else kernel_size//2+1
-        stride2 = stride
-        if pre_norm:
-            self.conv_x2 = nn.Sequential(
-                NORM[dim][norm](num_group, in_channels) if norm == 'group' else NORM[dim][norm](in_channels),
-                ACT[act](inplace=True),
-                CONV[dim](in_channels, out_channels, kernel_size, stride1, padding, bias=bias),
-                DROPOUT[dim](dropout,True),
-                NORM[dim][norm](num_group, out_channels) if norm == 'group' else NORM[dim][norm](out_channels),
-                ACT[act](inplace=True),
-                CONV[dim](out_channels, out_channels, kernel_size, stride2, padding, bias=bias),
-                DROPOUT[dim](dropout,True),
-            )
-        else:
-            self.conv_x2 = nn.Sequential(
-                CONV[dim](in_channels, out_channels, kernel_size, stride1, padding, bias=bias),
-                DROPOUT[dim](dropout,True),
-                NORM[dim][norm](num_group, out_channels) if norm == 'group' else NORM[dim][norm](out_channels),
-                ACT[act](inplace=True),
-                CONV[dim](out_channels, out_channels, kernel_size, stride2, padding, bias=bias),
-                DROPOUT[dim](dropout,True),
-                NORM[dim][norm](num_group, out_channels) if norm == 'group' else NORM[dim][norm](out_channels),
-                ACT[act](inplace=True),
-            )
+                
+        self.dconv_down1 = double_conv(1, 64)
+        self.dconv_down2 = double_conv(64, 128)
+        self.dconv_down3 = double_conv(128, 256)
+        self.dconv_down4 = double_conv(256, 512)        
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        if self.res:
-            return x + self.conv_x2(x)
-        return self.conv_x2(x)
+        self.maxpool = nn.MaxPool2d(2)
+        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)        
+        
+        self.dconv_up3 = double_conv(256 + 512, 256)
+        self.dconv_up2 = double_conv(128 + 256, 128)
+        self.dconv_up1 = double_conv(128 + 64, 64)
+        
+        self.conv_last = nn.Conv2d(64,1, 1)
 
-    def _check_param(self, dim: int, act: str, norm: str) -> None:
-        if dim != 2 and dim != 3:
-            raise ValueError(f"Convolution Dim:{dim} is unsupported!")
-        if act not in ["relu", "leaky","prelu"]:
-            raise ValueError(f"Activation Type:{act} is unsupported!")
-        if norm not in ["batch", "instance", "group"]:
-            raise ValueError(f"Normalization Type:{norm} is unsupported!")
+        self.e1_e3 = nn.Conv2d(64, 256, kernel_size=3, stride=2, padding=1)
+        self.conv_mid2 = nn.Conv2d(512, 256, kernel_size=3, stride=1, padding=1)
 
+        self.e2_e4 = nn.Conv2d(128, 512, kernel_size=3, stride=2, padding=1)
+        self.conv_mid1 = nn.Conv2d(1024, 512, kernel_size=3, stride=1, padding=1)
 
-class SKipUNet(nn.Module):
-    '''
-    UNet 3+: A full-scale connected unet for medical image segmentation
-    <https://arxiv.org/ftp/arxiv/papers/2004/2004.08790.pdf>
-    '''
-    def __init__(
-            self,
-            in_channels: int=1,
-            num_classes: int=1,
-            bias: bool = False,
-            n_filters: list = [ 64, 128, 256, 512],
-            deep_supervision: bool = False
-    ) -> None:
-        super().__init__()
-        self.deep_supervision = deep_supervision
-        C = n_filters[0]*4
+        self.e4_d2 = nn.Upsample(scale_factor=4, mode='nearest')
+        self.dec_mid1 = nn.Conv2d(512, 128, kernel_size=3, stride=1, padding=1)
 
-        self.encoder1 = Block(in_channels, n_filters[0], 2, bias=bias)
-        self.encoder2 = Block(n_filters[0], n_filters[1], 2, bias=bias)
-        self.encoder3 = Block(n_filters[1], n_filters[2], 2, bias=bias)
-        self.encoder4 = Block(n_filters[2], n_filters[3], 2, bias=bias)
-        #self.encoder5 = Block(n_filters[3], n_filters[4], 2, bias=bias)
+        self.d3_d1 = nn.Upsample(scale_factor=4, mode='nearest')
+        self.dec_mid2 = nn.Conv2d(256, 64, kernel_size=3, stride=1, padding=1)
+        
+        self.down1 = nn.Conv2d(256, 128, kernel_size=3, stride=1, padding=1)
+        self.down2 = nn.Conv2d(128, 64, kernel_size=3, stride=1, padding=1)
+        
+    def forward(self, x):
+        conv1 = self.dconv_down1(x)
+        x = self.maxpool(conv1)
 
-        self.pool = nn.MaxPool2d(2, 2, ceil_mode=True)
-
-        self.decoder1 = Block(C, C, 2, bias=bias)
-        self.decoder2 = Block(C, C, 2, bias=bias)
-        self.decoder3 = Block(C, C, 2, bias=bias)
-        self.decoder4 = Block(C, C, 2, bias=bias)
-        #self.decoder5 = Block(C, C, 2, bias=bias)
-
-        # Full-Scale Skip Connection
-        self.e1_d1 = SkipConv(n_filters[0], n_filters[0], bias=bias)
-        self.e1_d2 = SkipConv(n_filters[0], n_filters[0], True, False, 2, bias)
-        self.e1_d3 = SkipConv(n_filters[0], n_filters[0], True, False, 4, bias)
-        #self.e1_d4 = SkipConv(n_filters[0], n_filters[0], True, False, 8, bias)
-        self.e2_d2 = SkipConv(n_filters[1], n_filters[0], bias=bias)
-        self.e2_d3 = SkipConv(n_filters[1], n_filters[0], True, False, 2, bias)
-        #self.e2_d4 = SkipConv(n_filters[1], n_filters[0], True, False, 4, bias)
-        self.e3_d3 = SkipConv(n_filters[2], n_filters[0], bias=bias)
-        #self.e3_d4 = SkipConv(n_filters[2], n_filters[0], True, False, 2, bias)
-        #self.e4_d4 = SkipConv(n_filters[3], n_filters[0], bias=bias)
-
-        #self.e5_d1 = SkipConv(n_filters[4], n_filters[0], False, True, 16, bias)
-        self.e4_d1 = SkipConv(n_filters[3], n_filters[0], False, True, 8, bias)
-        self.e4_d2 = SkipConv(n_filters[3], n_filters[0], False, True, 4, bias)
-        self.e4_d3 = SkipConv(n_filters[3], n_filters[0], False, True, 2, bias)
-
-        #self.d4_d1 = SkipConv(C, n_filters[0], False, True, 8, bias)
-        #self.d4_d2 = SkipConv(C, n_filters[0], False, True, 4, bias)
-        #self.d4_d3 = SkipConv(C, n_filters[0], False, True, 2, bias)
-
-        self.d3_d1 = SkipConv(C, n_filters[0], False, True, 4, bias)
-        self.d3_d2 = SkipConv(C, n_filters[0], False, True, 2, bias)
-
-        self.d2_d1 = SkipConv(C, n_filters[0], False, True, 2, bias)
-
-        # Output Conv Layer in the official code uses 3 as kernel_size
-        if deep_supervision:
-            self.out1 = nn.Conv2d(C, num_classes, 3, 1, 1, bias=bias)
-            self.out2 = nn.Conv2d(C, num_classes, 3, 1, 1, bias=bias)
-            self.out3 = nn.Conv2d(C, num_classes, 3, 1, 1, bias=bias)
-            #self.out4 = nn.Conv2d(C, num_classes, 3, 1, 1, bias=bias)
-        else:
-            self.out = nn.Conv2d(C, num_classes, 3, 1, 1, bias=bias)
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        e1 = self.encoder1(x)
-        e2 = self.encoder2(self.pool(e1))
-        e3 = self.encoder3(self.pool(e2))
-        e4 = self.encoder4(self.pool(e3))
-        #e5 = self.encoder5(self.pool(e4))
-
-        # Fuse Decoder 4
-        #t1 = self.e1_d4(e1)
-        #t2 = self.e2_d4(e2)
-        #t3 = self.e3_d4(e3)
-        #t4 = self.e4_d4(e4)
-        #t5 = self.e5_d4(e5)
-        #fusion = torch.cat([t1, t2, t3, t4, t5], dim=1)
-        #d4 = self.decoder4(fusion)
-
-        # Fuse Decoder 3
-        t1 = self.e1_d3(e1)
-        t2 = self.e2_d3(e2)
-        t3 = self.e3_d3(e3)
-        t4 = self.e4_d3(e4)
-        #t5 = self.e5_d3(e5)
-        fusion = torch.cat([t1, t2, t3, t4], dim=1)
-        d3 = self.decoder3(fusion)
-
-        # Fuse Decoder 2
-        t1 = self.e1_d2(e1)
-        t2 = self.e2_d2(e2)
-        t3 = self.e4_d2(e4)
-        t4 = self.d3_d2(d3)
-        # t3 = self.d3_d2(d3)
-        # t4 = self.d4_d2(d4)
-        # t5 = self.e5_d2(e5)
-        fusion = torch.cat([t1, t2, t3, t4], dim=1)
-        d2 = self.decoder2(fusion)
-
-        # Fuse Decoder 1
-        t1 = self.e1_d1(e1)
-        t2 = self.d2_d1(d2)
-        t3 = self.d3_d1(d3)
-        #t4 = self.d4_d1(d4)
-        t4 = self.e4_d1(e4)
-        fusion = torch.cat([t1, t2, t3, t4], dim=1)
-        d1 = self.decoder1(fusion)
-
-        if self.deep_supervision:
-            out1 = self.out1(d1)
-            out2 = self.out2(d2)
-            out3 = self.out3(d3)
-            
-            return [out1, out2, out3]
-        else:
-            return self.out(d1)
+        conv2 = self.dconv_down2(x)
+        x = self.maxpool(conv2)
+        
+        conv3 = self.dconv_down3(x)
+        x = self.maxpool(conv3) 
 
 
-class SkipConv(nn.Module):
-    def __init__(
-        self,
-        in_channels: int,
-        out_channels: int,
-        pool: bool = False,
-        up: bool = False,
-        scale_factor: int = 2,
-        bias: bool = False,
-    ) -> None:
-        '''Full-scale Skip Connections in the paper'''
-        super().__init__()
-        assert (pool and up) == False, "Skip connection should be only downsampling or upsampling"
-        self.pool = pool
-        self.up = up
-        if pool:
-            self.downsample = nn.MaxPool2d(scale_factor, scale_factor, ceil_mode=True)
-        if up:
-            self.upsample = nn.Upsample(scale_factor=scale_factor, mode='bilinear', align_corners=True)
-        self.conv = nn.Conv2d(in_channels, out_channels, 3, 1, 1, bias=bias)
-        self.bn = nn.BatchNorm2d(out_channels)
-        self.relu = nn.ReLU(True)
+        x = self.dconv_down4(x)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        if self.pool:
-            x = self.downsample(x)
-        elif self.up:
-            x = self.upsample(x)
-        x = self.relu(self.bn(self.conv(x)))
-        return x
+        mid = self.e2_e4(conv2)
+        mid = self.maxpool(mid)
+        x = torch.cat([mid,x], dim=1)
+        x = self.conv_mid1(x)
+
+        dec_1 = x
+        dec_1 = self.e4_d2(dec_1)
+        dec_1 = self.dec_mid1(dec_1)
+
+
+        x = self.upsample(x)
+
+        mid = self.e1_e3(conv1)
+        mid = self.maxpool(mid)
+        mid = torch.cat([conv3,mid], dim=1)
+        mid = self.conv_mid2(mid)
+
+        x = torch.cat([x, mid], dim=1)
+        
+        x = self.dconv_up3(x)
+
+        dec_2 = x
+        dec_2 = self.d3_d1(dec_2)
+        dec_2 = self.dec_mid2(dec_2)
+
+        x = self.upsample(x)   
+        x = self.down1(x)
+        x = torch.cat([x, dec_1], dim=1) 
+        print("x1",x.shape)
+       
+
+        x = torch.cat([x, conv2], dim=1)       
+        x = self.dconv_up2(x)
+
+        x = self.upsample(x) 
+        x = self.down2(x)
+
+        x = torch.cat([x,dec_2], dim=1)
+        
+        print("x2",x.shape)
+
+        x = torch.cat([x, conv1], dim=1)   
+        
+        x = self.dconv_up1(x)
+        
+        out = self.conv_last(x)
+        
+        return out
+
+if __name__ == "__main__":
+  
+  x = ModifiedNet()
+  print(sum(p.numel() for p in x.parameters()))
+  print(x(torch.randn(1,1,128,128)).shape)
